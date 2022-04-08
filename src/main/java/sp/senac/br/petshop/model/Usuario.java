21832f7a99ffd0c8dfd890d609813260600b0945
Ruunes <ruan0432000@gmail.com>
2022-04-08 20:04:30 -0300

    Criando classe de validação de segurança para criptografia de senhas

--- src/main/java/sp/senac/br/petshop/model/Usuario.java
+++ src/main/java/sp/senac/br/petshop/model/Usuario.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 //import sp.senac.br.pet.SecurityConfig;
 //import sp.senac.br.pet.constraint.FieldMatch;
 
+import sp.senac.br.petshop.Validator.SecurityConfig;
+
 import javax.persistence.*;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
@@ -111,94 +113,180 @@ public abstract class Usuario implements UserDetails
     {
         return idUsuario;
     }
+
     public void setIdUser(Long idUser) 
     {
         this.idUsuario = idUser;
     }
+
     public String getName() 
     {
         return nome;
     }
+
     public void setName(String name) 
     {
         this.nome = name;
     }
+
     public String getSobrenome() 
     {
         return sobrenome;
     }
+
+
+
     public void setSobrenome(String sobrenome) 
     {
         this.sobrenome = sobrenome;
     }
+
+
     public String getCPF() 
     {
         return CPF;
     }
+
+
     public void setCPF(String cpf) 
     {
         CPF = cpf;
     }
+
+
     public Date getDataNascimento() 
     {
         return dataNascimento;
     }
+
+
     public void setDataNascimento(Date dataNascimento) 
     {
         this.dataNascimento = dataNascimento;
     }
+
+
     public String getTelefone() 
     {
         return telefone;
     }
+
+    
     public void setTelefone(String telefone)
     {
         this.telefone = telefone;
     }
+
     public String getEmail() 
     {
         return email;
     }
+
+
     public void setEmail(String email) 
     {
         this.email = email;
     }
+
+
     public String getConfirmarEmail() 
     {
         return ConfirmarEmail;
     }
+
+
     public void setConfirmarEmail(String confirmarEmail) 
     {
         this.ConfirmarEmail = confirmarEmail;
     }
+
+
     public String getHashSenha()
     {
         return hashSenha;
     }
+
+
     public void  setHashSenha(String hashsenha)
     {
         this.hashSenha = hashsenha;
     }
+
 
     public String getSenha() 
     {
         return senha;
     }
 
+
     public void setSenha(String senha) 
     {
-        this.senha = senha;
+        this.hashSenha =
+                SecurityConfig.bcryptPasswordEncoder()
+                        .encode(senha);
     }
+
+
+    public void setCsenha(String csenha) 
+    {
+        this.csenha = SecurityConfig.bcryptPasswordEncoder()
+                .encode(csenha);
+    }
+
+
 
     public String getConfirmarSenha() 
     {
         return ConfirmarSenha;
     }
+
     
     public void setConfirmarSenha(String confirmarSenha) 
     {
         ConfirmarSenha = confirmarSenha;
     }
+    
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() 
+    {
+        return null;
+    }
 
+    @Override
+    public String getPassword() 
+    {
+        return getHashSenha();
+    }
+
+    @Override
+    public String getUsername() 
+    {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() 
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() 
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() 
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() 
+    {
+        return true;
+    }
     
 }
